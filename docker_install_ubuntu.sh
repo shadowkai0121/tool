56497deac30a7da871992b35241d39dafcd51e52
KaiHsieh <shadowkai0121@gmail.com>
2023-11-28 15:40:07 +0800

    Update docker_install_ubuntu.sh

--- docker_install_ubuntu.sh
+++ docker_install_ubuntu.sh
@@ -16,7 +16,6 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
 groups $USER
 
 echo $(docker --version)
