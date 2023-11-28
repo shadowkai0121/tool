c4cd2a92ca304682fc8367c23b0cc649a80d3af7
KaiHsieh <shadowkai0121@gmail.com>
2023-11-28 15:30:51 +0800

    Update docker_dind.sh

--- docker_dind.sh
+++ docker_dind.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run --privileged --name dind -d docker:stable-dind
-docker exec -it dind /bin/ash && docker rm -f dind
+docker run --privileged --name dind --rm -i docker:stable-dind /bin/bash
