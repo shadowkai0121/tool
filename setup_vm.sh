d2de1be56397f35d66c03b85acdb9dcb91f251dc
KaiHsieh <shadowkai0121@gmail.com>
2023-12-08 14:26:59 +0800

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt-get update -y --upgrade --fix-missing
+sudo apt-get install -y build-essential module-assistant
 sudo mkdir -p /media/cdrom
 sudo mount /dev/cdrom /media/cdrom/
 sudo /media/cdrom/VBoxLinuxAdditions.run
