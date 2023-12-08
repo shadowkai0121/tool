4b2cf7ef541d5cb38276503e27b9ad55ad337f0a
KaiHsieh <shadowkai0121@gmail.com>
2023-12-08 14:27:16 +0800

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update -y --upgrade --fix-missing
+sudo apt-get update -y --upgrade --fix-missing
 sudo apt-get install -y build-essential module-assistant
 sudo mkdir -p /media/cdrom
 sudo mount /dev/cdrom /media/cdrom/
