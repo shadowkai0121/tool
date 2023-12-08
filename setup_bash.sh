b64218ac5366dc467490b41a991d1b4d24bf689d
KaiHsieh <shadowkai0121@gmail.com>
2023-12-08 12:31:01 +0800

    Update setup_bash.sh

--- setup_bash.sh
+++ setup_bash.sh
@@ -2,7 +2,10 @@
 CURRENT_PATH=$(grep "PATH" ~/.bashrc)
 
 BASHRC_URL="https://raw.githubusercontent.com/shadowkai0121/tool/master/.bashrc"
-curl -s "$NEW_BASHRC_URL" > ~/.bashrc
+curl -H "Cache-Control: no-cache, no-store, must-revalidate"\
+     -H "Pragma: no-cache"\
+     -H "Expires: 0"\
+     -s "$NEW_BASHRC_URL" > ~/.bashrc
 echo $CURRENT_PATH >> ~/.bashrc
 
 source ~/.bashrc
