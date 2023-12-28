391f1d6de85bb160dc4e98401e2fde2bbedfa24d
KaiHsieh <shadowkai0121@gmail.com>
2023-12-28 10:54:21 +0800

    Update ubuntu_sysbox_install.sh

--- ubuntu_sysbox_install.sh
+++ ubuntu_sysbox_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt install -y curl gnupg2 software-properties-common
+apt-get install -y curl gnupg2 software-properties-common
 curl -s https://packagecloud.io/install/repositories/nestybox/sysbox/script.deb.sh | bash
-apt install sysbox-runc sysbox-fs sysbox-mgr
+apt-get install sysbox-runc sysbox-fs sysbox-mgr
